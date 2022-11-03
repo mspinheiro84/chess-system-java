a2a0648bc3d42248773093b5f0838d068c57a652
Marcelo Pinheiro <pinheiro84@gmail.com>
2022-11-02 23:05:16 -0300

    Movimento especial - Promoção

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,12 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if (chessMatch.getPromoted() != null){
+					System.out.print("Enter  piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
@@ -48,7 +54,7 @@ public class Program {
 			}
 		}
 		UI.clearScreen();
-		UI.printMatch(chessMatch, captured);
+		UI.printMatch(chessMatch, captured);	
 	}
 
 }
